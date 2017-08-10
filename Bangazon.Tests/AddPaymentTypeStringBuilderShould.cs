14a00f89574352979a3a2f2885973fa9c4c06ccf
Kathleen Weisensel <kweisensel@gmail.com>
2017-08-10 11:40:04 -0500

    why doesnt this work

--- Bangazon.Tests/AddPaymentTypeStringBuilderShould.cs
+++ Bangazon.Tests/AddPaymentTypeStringBuilderShould.cs
@@ -7,20 +7,22 @@ namespace Bangazon.Tests
 {
     public class AddPaymentTypeStringBuilderManagerShould
     {
-    private AddPaymentTypeStringBuilder kathy;
+    private DatabaseInterface _db;
+    private AddPaymentTypeStringBuilder _kathy;
     public AddPaymentTypeStringBuilderManagerShould()
     {
-
+        _db = new DatabaseInterface("BANGAZONCLI_DB");
+        _db.CheckPaymentTypeTable();
+        _kathy = new AddPaymentTypeStringBuilder(_db);
     }
     [Fact]
     public void AddPaymentTypeReturnInt()
     {   
         PaymentType _paytype = new PaymentType("Visa", 01234958585);
         
-        var X = kathy.AddPaymentType(_paytype);
+        var X = _kathy.AddPaymentType(_paytype);
         
-        Assert.True(X != 0);  
-        Console.WriteLine(X);   
+        Assert.True(X != 0);     
     }
     }
 }
