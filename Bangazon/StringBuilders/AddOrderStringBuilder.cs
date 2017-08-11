0d690a3dcb9a6661f9adecb1aa9c712f704fc243
Kathleen Weisensel <kweisensel@gmail.com>
2017-08-11 14:38:21 -0500

    added comments for future code placement

--- Bangazon/StringBuilders/AddOrderStringBuilder.cs
+++ Bangazon/StringBuilders/AddOrderStringBuilder.cs
@@ -7,12 +7,16 @@ namespace Bangazon.StringBuilders
 {
     public class AddOrderMenu
     {
+
+        // add product to customer's cart
         public void AddOrderStringBuilder(OrderManager SB)
         {
         Order order = new Order();
         var counter = 1;
         Console.Clear();
         Console.WriteLine ("Choose Product:");
+        List<Product>products = null;
+        // add function once it is created to pull list of products here
         foreach (var product in products)
             {
                 Console.WriteLine($"{counter++} {product.Name}");
